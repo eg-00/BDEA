68fa0ed509cc8aaffb004a32492eb7d3924e87dc
Emil Gäng <emilgaeng@web.de>
2023-05-03 11:03:08 +0200

    added nlp

--- complete/src/main/java/com/example/uploadingfiles/FileUploadController.java
+++ complete/src/main/java/com/example/uploadingfiles/FileUploadController.java
@@ -1,31 +1,38 @@
 package com.example.uploadingfiles;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
-
+import com.example.uploadingfiles.storage.StorageFileNotFoundException;
+import com.example.uploadingfiles.storage.StorageService;
+import com.kennycason.kumo.CollisionMode;
+import com.kennycason.kumo.WordCloud;
+import com.kennycason.kumo.WordFrequency;
+import com.kennycason.kumo.bg.CircleBackground;
+import com.kennycason.kumo.font.scale.SqrtFontScalar;
+import com.kennycason.kumo.nlp.FrequencyAnalyzer;
+import com.kennycason.kumo.palette.ColorPalette;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.example.uploadingfiles.storage.StorageFileNotFoundException;
-import com.example.uploadingfiles.storage.StorageService;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class FileUploadController {
 
+
 	private final StorageService storageService;
+	public final static String CLOUD_PATH = "tagclouds/";
 
 	@Autowired
 	public FileUploadController(StorageService storageService) {
@@ -68,4 +75,35 @@ public class FileUploadController {
 		return ResponseEntity.notFound().build();
 	}
 
+	private String[] listTagClouds() {
+		File[] files = new File(CLOUD_PATH).listFiles();
+		String[] clouds = new String[files.length];
+
+		for (int i = 0; i < files.length; i++) {
+			String name = files[i].getName();
+			if (files[i].getName().endsWith(".png"))
+				clouds[i] = CLOUD_PATH + name;
+		}
+
+		return clouds;
+	}
+
+	private void createTagCloud(String filename, String inhalt) throws IOException {
+		final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+		frequencyAnalyzer.setWordFrequenciesToReturn(300);
+		frequencyAnalyzer.setMinWordLength(4);
+
+		List<String> texts = new ArrayList<>();
+		texts.add(inhalt);
+		final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load(texts);
+
+		final Dimension dimension = new Dimension(600, 600);
+		final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
+		wordCloud.setPadding(2);
+		wordCloud.setBackground(new CircleBackground(300));
+		wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
+		wordCloud.setFontScalar(new SqrtFontScalar(8, 50));
+		wordCloud.build(wordFrequencies);
+		wordCloud.writeToFile(CLOUD_PATH + filename + ".png");
+	}
 }
